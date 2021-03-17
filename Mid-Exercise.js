00b1d53deb9901909f0967f2cd5b154084dca8b2
DorinMickel <80397848+DorinMickel@users.noreply.github.com>
2021-03-17 11:16:54 +0200

    examination

--- Mid-Exercise.js
+++ Mid-Exercise.js
@@ -23,20 +23,21 @@ function submitContactUs() {
 }
 
 
-var changeImage = setInterval(changeImage,8000);
+var changeImage = setInterval(changeImage,5000);
 
 function changeImage() {
     var image = document.getElementById('changingImage').src;
+    console.log(image)
 
-    if (image == "http://127.0.0.1:5501/paz_250x110103131473.jpg") {
-    document.getElementById('changingImage').src = "http://127.0.0.1:5501/250X110668568400.jpg";
+    // if (image == "http://127.0.0.1:5501/paz_250x110103131473.jpg") {
+    // document.getElementById('changingImage').src = "http://127.0.0.1:5501/250X110668568400.jpg";
 
-    } else if (image == "http://127.0.0.1:5501/250X110668568400.jpg") {
-        document.getElementById('changingImage').src = "http://127.0.0.1:5501/250X110571325734.jpg";
+    // } else if (image == "http://127.0.0.1:5501/250X110668568400.jpg") {
+    //     document.getElementById('changingImage').src = "http://127.0.0.1:5501/250X110571325734.jpg";
 
-    } else {
-        document.getElementById('changingImage').src = "http://127.0.0.1:5501/paz_250x110103131473.jpg";
-    }
+    // } else {
+    //     document.getElementById('changingImage').src = "http://127.0.0.1:5501/paz_250x110103131473.jpg";
+    // }
 }
 
 var changeHeadline = setInterval(changeHeadline,5000);
